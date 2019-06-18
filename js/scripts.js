d40c7fb163d11d1358b628a3268ecc8c13eb2531
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-06-18 17:18:54 +0300

    Update project files

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-var popup = document.querySelector(".popup");
-var menuButton = document.querySelector(".header__menu");
-
-menuButton.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  popup.classList.toggle("popup__show");
-});
