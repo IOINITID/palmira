b32bcebcbcfe7e3a325f820ba29ce78e744f3e3f
IOINITID <50173108+IOINITID@users.noreply.github.com>
2019-06-19 16:21:57 +0300

    Update project

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
+//Tabs actions
 $(function () {
   $("#tabs").tabs();
 });
-
+//Tabs actions
 $(".item-description").click(() => {
   $(".item-description").addClass("item-active");
   $(".item-specs").removeClass("item-active");
 });
-
+//Tabs actions
 $(".item-specs").click(() => {
   $(".item-specs").addClass("item-active");
   $(".item-description").removeClass("item-active");
 });
+//Images actions
+function l_image(a) {
+  document.largeIMG.src = a
+}
